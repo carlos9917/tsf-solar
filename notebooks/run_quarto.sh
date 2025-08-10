61566acf4ad1174fa65eee3b88d2297e617072d5
Carlos Peralta <carlos9917@gmail.com>
2025-08-10 18:01:07 +0200

    Removed all notebooks

--- notebooks/run_quarto.sh
+++ notebooks/run_quarto.sh
@@ -2,14 +2,25 @@
 
 # This script renders the Quarto notebooks for the case study.
 
-echo "Rendering notebook for Question 1 (Python)..."
-quarto render eda_question_1.py.qmd --to html
+echo "Rendering EDA notebook for Question 1..."
+quarto render eda_question_1.qmd --to html
 
-echo "Rendering notebook for Question 2 (Python)..."
-quarto render eda_question_2.py.qmd --to html
+echo "Rendering modelling notebook for Question 1..."
+quarto render model_evaluation_question_1.qmd --to html
 
+echo "Rendering answer notebook for Question 1..."
+quarto render answer_question_1.qmd --to html
+
+echo "Rendering EDA notebook for Question 2..."
+quarto render eda_question_2.qmd --to html
+
+echo "Rendering modelling notebook for Question 2..."
+quarto render model_evaluation_question_2.qmd --to html
+
+echo "Rendering answer notebook for Question 2..."
+quarto render answer_question_2.qmd --to html
 echo "Done."
 
 # convert to pdf
-quarto render eda_question_1.py.qmd --to pdf
-quarto render eda_question_2.py.qmd --to pdf
+#quarto render eda_question_1.py.qmd --to pdf
+#quarto render eda_question_2.py.qmd --to pdf
